14ea2f6799395c12e96ea6cec01f527875686ca1
mdnahidislam <mdnahidislam30@gmail.com>
2022-08-09 18:14:57 +0600

    React router declared. Pages and components will creating soon.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,75 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { FiAlignCenter } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 import './App.css';
 
 function App() {
+  const activeMenu = true;
+
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4 " style={{ zIndex: '1000' }}>
+          <TooltipComponent content="Settings" position="top">
+            <button
+              type="button"
+              className="text-3xl p-2 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              style={{ background: 'blue', borderRadius: '50%' }}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg">
+            Sidebar
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+        )}
+        <div
+          className={`bg-main-bg dark:bg-main-bg min-h-screen w-full ${
+            activeMenu ? 'md:ml-72' : 'flex-2'
+          }`}
+        >
+          <div className="fixed md:static navbar w-full bg-main-bg dark:bg-main-dark-bg">
+            NavBar
+          </div>
+        </div>
+
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path='/' element="ECommerce" />
+            <Route path='/ecommerce' element="Home" />
+
+            {/* Pages */}
+            <Route path='/order' element="Order" />
+            <Route path='/employees' element="employees" />
+            <Route path='/customers' element="customers" />
+
+            {/* Apps */}
+            <Route path='/kanban' element="kanban" />
+            <Route path='/editor' element="editor" />
+            <Route path='/calendar' element="calender" />
+            <Route path='/color-picker' element="color-picker" />
+
+            {/* Apps */}
+            <Route path='/line' element="line" />
+            <Route path='/area' element="area" />
+            <Route path='/bar' element="bar" />
+            <Route path='/chart' element="chart" />
+            <Route path='/pie' element="pie" />
+            <Route path='/color-mapping' element="color-mapping" />
+            <Route path='/pyramid' element="pyramid" />
+            <Route path='/stacked' element="stacked" />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdk1jUX1dc3RRQ2JfVEE=');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
