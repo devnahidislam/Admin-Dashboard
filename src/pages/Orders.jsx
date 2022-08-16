96fe4c06ac0e569572a688bdaadbe97ec15a885e
mdnahidislam <mdnahidislam30@gmail.com>
2022-08-16 23:55:40 +0600

    Order pages table design completed and some functionality will add shortly.

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import {
+  ColumnsDirective,
+  ColumnDirective,
+  GridComponent,
+} from '@syncfusion/ej2-react-grids';
+import React from 'react';
+import { Header } from '../components';
+import { ordersData, ordersGrid } from '../data/dummy';
 
 const Orders = () => {
   return (
-    <div>
-      Orders
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl ">
+      <Header title="Order" category="Page" />
+      <GridComponent id="grid-comp" dataSource={ordersData} allowPaging>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
