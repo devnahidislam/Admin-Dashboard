d633c160aa8576b76e5ac0573cff4fe71d9fee14
mdnahidislam <mdnahidislam30@gmail.com>
2022-08-17 10:48:54 +0600

    Pagination and sorting added in order table

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,6 +2,15 @@ import {
   ColumnsDirective,
   ColumnDirective,
   GridComponent,
+  Inject,
+  Resize,
+  Sort,
+  Page,
+  Filter,
+  ContextMenu,
+  ExcelExport,
+  Edit,
+  PdfExport,
 } from '@syncfusion/ej2-react-grids';
 import React from 'react';
 import { Header } from '../components';
@@ -11,12 +20,24 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl ">
       <Header title="Order" category="Page" />
-      <GridComponent id="grid-comp" dataSource={ordersData} allowPaging>
+      <GridComponent id="grid-comp" dataSource={ordersData} allowPaging allowSorting allowPdfExport>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            Page,
+            Filter,
+            ContextMenu,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   );
